fdba7d695ff9f703fea6b01c53b2c564f4bf83b1
ylland_IT2 <ylland_IT2@ylland_IT2-PC>
2016-12-19 14:24:52 +0900

    20161219

---
+++ BaekjoonAlgorithm/src/BOJ_20161219/BOJ10871.java
@@ -0,0 +1,32 @@
+package BOJ_20161219;
+
+import java.util.Scanner;
+
+public class BOJ10871 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String a = scanner.nextLine();
+		String b = scanner.nextLine();
+		String result = null;
+		int comparison = Integer.parseInt(a.split(" ")[1]);
+		
+		boolean flag = false;  
+		String[] value = b.split(" ");
+		
+		
+		for(String str : value){
+			if(Integer.parseInt(str)<comparison){
+				if(flag == false){
+					result=str;
+					flag=true;
+				}else{
+					result+=" " + str;
+				}
+			}
+		}
+		System.out.println(result);
+	}
+	
+}
